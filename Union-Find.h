001bb03cedbf9c86a27c9c319ddc0527986deccc
georgez10 <georgez.1996@hotmail.com>
2019-01-07 00:23:04 +0200

    b33333

--- Union-Find.h
+++ Union-Find.h
@@ -2,15 +2,17 @@
 #define MEVNI3WET2_UNION_FIND_H
 
 class UnionFind{
-    int *parent, counter, *size;
-
+    int* parent;
+    int counter;
+    int* size;
 public:
-    UnionFind(int N) {
+    UnionFind(int N){
         counter = N;
         parent = new int[N];
         size = new int[N];
         for (int i = 0; i<N; i++) {
-            parent[i] = i, size[i] = 1;
+            parent[i] = i;
+            size[i] = 1;
         }
     }
     ~UnionFind() {
@@ -19,7 +21,7 @@ public:
     }
     int find(int p) {
         int root = p;
-        while (root != parent[root])    root = parent[root];
+        while (root != parent[root]) root = parent[root];
         while (p != root) {
             int newp = parent[p];
             parent[p] = root;
@@ -27,15 +29,25 @@ public:
         }
         return root;
     }
-    void merge(int x, int y) {
+    void merge(int x, int y){
         int i = find(x);
         int j = find(y);
         if (i == j) return;
-        if (size[i] < size[j]) { parent[i] = j, size[j] += size[i]; }
-        else { parent[j] = i, size[i] += size[j]; }
+        if (size[i] < size[j]){
+            parent[i] = j;
+            size[j] += size[i];
+        }
+        else {
+            parent[j] = i;
+            size[i] += size[j];
+        }
         counter--;
     }
-    bool connected(int x, int y) { return find(x) == find(y); }
-    int count() { return counter; }
+    bool connected(int x, int y){
+        return find(x) == find(y);
+    }
+    int count(){
+        return counter;
+    }
 };
 #endif //MEVNI3WET2_UNION_FIND_H
