51c15b5ca414e2ed86074d99fd13c8d99760023f
georgez10 <georgez.1996@hotmail.com>
2019-01-06 18:39:37 +0200

    b33333

---
+++ AVLtree.h
@@ -0,0 +1,552 @@
+
+#ifndef WET1_AVLTREE_H
+#define WET1_AVLTREE_H
+
+#include <functional>
+#include <iostream>
+#include <math.h>
+#include "exceptions.h"
+
+using namespace Wet1::AVL;
+
+template <class T, class K>
+class Node{
+public:
+    T data;
+    K key;
+    Node* rightSon;
+    Node* leftSon;
+    Node* parent;
+    int balance;
+    int height;
+    Node(const T data, const K key);
+    Node(const Node&) = default;
+    ~Node() = default;
+};
+
+template <class T, class K>
+class PutInTree {
+    int index;
+    T* arr;
+public:
+    PutInTree(T* arr) : index(0), arr(arr) {};
+    ~PutInTree() = default;
+    void operator() (T& data, K& key){
+        data = arr[index];
+        key = arr[index];
+        index++;
+    }
+};
+
+template <class Filter, class T, class K>
+class PutInArray {
+    Filter filter;
+    int index;
+    T* arr;
+public:
+    PutInArray(Filter& filter,int size) : filter(filter), index(0) {
+        arr = new T[size];
+    }
+    ~PutInArray() {
+        delete[] arr;
+    }
+    void operator() (T& data, K& key){
+        if (filter(data)) {
+            arr[index] = data;
+            index++;
+        }
+    }
+    T* GetArray(){
+        return arr;
+    }
+};
+
+template <class Filter, class T, class K>
+class Counter{
+    int count;
+    Filter filter;
+public:
+    Counter(Filter& filter) : count(0), filter(filter){};
+    ~Counter() = default;
+    void operator() (T& data, K& key){
+        if (filter(data)) count++;
+    }
+    int GetCounter(){
+        return count;
+    }
+};
+
+template <class T, class K>
+static void Fixheight (Node<T,K>* root) {
+    if (root) {
+        int leftHeight = 0;
+        int rightHeight = 0;
+        if (root->leftSon != NULL) {
+            leftHeight = root->leftSon->height;
+        }
+        if (root->rightSon != NULL) {
+            rightHeight = root->rightSon->height;
+        }
+        if (leftHeight > rightHeight) {
+            root->height = leftHeight + 1;
+            root->balance = leftHeight - rightHeight;
+            return;
+        }
+        root->height = rightHeight + 1;
+        root->balance = leftHeight - rightHeight;
+    }
+}
+
+template <class DoSomething, class T, class K>
+static void InOrderAux(Node<T,K>* root, DoSomething &doSomething){
+    if (root){
+        InOrderAux(root->leftSon,doSomething);
+        doSomething(root->data, root->key);
+        InOrderAux(root->rightSon,doSomething);
+    }
+}
+
+template <class DoSomething, class T, class K>
+static void ReverseInOrderAux(Node<T,K>* root, DoSomething &doSomething){
+    Node<T, K>* tmp;
+    if (root){
+        tmp = root->parent;
+        ReverseInOrderAux(root->rightSon, doSomething);
+        if(doSomething(root)){
+            Fixheight(tmp);
+            return;
+        }
+        ReverseInOrderAux(root->leftSon, doSomething);
+        Fixheight(tmp);
+    }
+}
+
+template <class T>
+static void merge1(T* a, int na, T* b, int nb, T* c){
+    int ia, ib, ic;
+    for(ia=ib=ic=0; (ia < na) && (ib < nb); ic++){
+        if (a[ia] < b[ib]) {
+            c[ic] = a[ia];
+            ia++;
+        }
+        else if (a[ia] == b[ib]){
+            c[ic] = a[ia] + b[ib];
+            ib++;
+            ia++;
+        }
+        else{
+            c[ic] = b[ib];
+            ib++;
+        }
+    }
+    for(; ia < na; ia++, ic++) c[ic] = a[ia];
+    for(; ib < nb; ib++, ic++) c[ic] = b[ib];
+};
+
+template <class T, class K>
+class AVL{
+    Node<T,K>* head;
+    int size;
+public:
+    AVL();
+    AVL(const AVL&) = delete;
+    ~AVL();
+    int GetSize();
+    void Insert (const T& data, const K& key);
+    bool contains(const K key);
+    void remove(const K key);
+    T& Get(const K key);
+    Node<T,K>* GetNode(const K key);
+
+    template <class DoSomething>
+    void InOrder(DoSomething& doSomething) {
+        InOrderAux(head,doSomething);
+    }
+    template <class DoSomething>
+    void ReverseInOrder(DoSomething& doSomething ) {
+        ReverseInOrderAux(head, doSomething);
+    }
+    void EmptyTree (int size, Node<T,K>* dummy);
+    void BuildAlmostComplete(int size, Node<T,K>* dummy);
+    template <class Filter>
+    void uniteTrees(AVL* tree1, AVL* tree2, Filter& f){
+        Counter<Filter, T, K> counter1(f);
+        tree1->InOrder(counter1);
+        Counter<Filter, T, K> counter2(f);
+        tree2->InOrder(counter2);
+        PutInArray<Filter, T, K> putA1(f, counter1.GetCounter());
+        PutInArray<Filter, T, K> putA2(f, counter2.GetCounter());
+        tree1->InOrder(putA1);
+        tree2->InOrder(putA2);
+        int newSize = counter1.GetCounter() + counter2.GetCounter();
+        T* merged = new T[newSize];
+        merge1(putA1.GetArray(), counter1.GetCounter(), putA2.GetArray(), counter2.GetCounter(), merged);
+        Node<T, K>* dummy = (tree1->head) ? tree1->head : (tree2->head) ? tree2->head : tree1->head;
+        BuildAlmostComplete(newSize, dummy);
+        PutInTree<T, K> putIn(merged);
+        this->InOrder(putIn);
+        this->size = newSize;
+        delete[] merged;
+        delete tree1;
+        delete tree2;
+    }
+};
+
+// if node is parent's: right son it returns 1, left son 0, otherwise -1
+template <class T, class K>
+static int CheckWhichSonForParent(Node<T,K>* node){
+    if (!(node->parent)) return -1;
+    if (node->parent->rightSon == node) return 1;
+    return 0;
+}
+
+template <class T, class K>
+static void DeleteTree(Node<T,K>* head) {
+    if (head == NULL ){
+        return;
+    }
+    DeleteTree(head->leftSon);
+    DeleteTree(head->rightSon);
+    delete head;
+}
+
+template <class T, class K>
+Node<T,K>::Node(const T data, const K key) : data(data), key(key), rightSon(NULL), leftSon(NULL),
+                                        parent(NULL),  balance(0), height(1) {}
+
+template <class T, class K>
+AVL<T,K>::AVL() : head(NULL), size(0) {};
+
+template <class T, class K>
+AVL<T,K>::~AVL() {
+    DeleteTree(head);
+}
+
+
+template <class T, class K>
+static Node<T,K>* RotateLL(Node<T,K>* root) {
+    Node<T,K>* parent = root->parent;
+    int right = CheckWhichSonForParent(root);
+    Node<T,K>* temp = root->leftSon;
+    root->leftSon = temp->rightSon;
+    if (root->leftSon) root->leftSon->parent = root;
+    temp->rightSon = root;
+    root->parent = temp;
+    temp->parent = parent;
+    if (right != -1){
+        if (right == 1){
+            parent->rightSon = temp;
+        } else{
+            parent->leftSon = temp;
+        }
+    }
+    Fixheight(root);
+    Fixheight(temp);
+    return temp;
+}
+
+template <class T, class K>
+static Node<T,K>* RotateRR(Node<T,K>* root) {
+    Node<T,K>* parent = root->parent;
+    int right = CheckWhichSonForParent(root);
+    Node<T,K>* temp = root->rightSon;
+    root->rightSon = temp->leftSon;
+    if (root->rightSon) root->rightSon->parent = root;
+    temp->leftSon = root;
+    root->parent = temp;
+    temp->parent = parent;
+    if (right != -1){
+        if (right == 1){
+            parent->rightSon = temp;
+        } else{
+            parent->leftSon = temp;
+        }
+    }
+    Fixheight(root);
+    Fixheight(temp);
+    return temp;
+}
+
+template <class T, class K>
+static Node<T,K>* Balance (Node<T,K>* root){
+    Fixheight(root);
+    if (root->balance == 2){
+        if (root->leftSon->balance < 0){
+            root->leftSon = RotateRR(root->leftSon);
+        }
+        return RotateLL(root);
+    }
+    if (root->balance == -2){
+        if (root->rightSon->balance > 0){
+            root->rightSon = RotateLL(root->rightSon);
+        }
+        return RotateRR(root);
+    }
+    return root;
+}
+
+// if node has: 2 sons it returns 2, right son 1, left son 0, otherwise -1
+template <class T, class K>
+static int CheckSons(Node<T,K>* node){
+    if (!(node->rightSon) && !(node->leftSon)) return -1;
+    if ((node->rightSon) && !(node->leftSon)) return 1;
+    if (!(node->rightSon) && (node->leftSon)) return 0;
+    return 2;
+}
+
+template <class T, class K>
+static void SwitchNodes(Node<T,K>* node1, Node<T,K>* node2){
+    int parentsSon1 = CheckWhichSonForParent(node1);
+    int parentsSon2 = CheckWhichSonForParent(node2);
+    int height = node1->height;
+    int balance = node1->balance;
+    Node<T,K>* parent1 = node1->parent;
+    Node<T,K>* parent2 = node2->parent;
+    node1->height = node2->height;
+    node1->balance = node2->balance;
+    node2->height = height;
+    node2->balance = balance;
+    node1->parent = (node1 == parent2) ? node2 : parent2;
+    node2->parent = parent1;
+    if (parentsSon1 != -1){
+        (parentsSon1 == 1) ? parent1->rightSon = node2 : parent1->leftSon = node2;
+    }
+    if (parentsSon2 != -1){
+        (parentsSon2 == 1) ? parent2->rightSon = node1 : parent2->leftSon = node1;
+    }
+    Node<T,K>* tmp = node1->rightSon;
+    node1->rightSon = node2->rightSon;
+    node2->rightSon = tmp;
+    tmp = node1->leftSon;
+    node1->leftSon = node2->leftSon;
+    node2->leftSon = tmp;
+    if (node1->rightSon) node1->rightSon->parent = node1;
+    if (node1->leftSon) node1->leftSon->parent = node1;
+    if (node2->rightSon) node2->rightSon->parent = node2;
+    if (node2->leftSon) node2->leftSon->parent = node2;
+}
+
+template <class T, class K>
+static Node<T,K>* FindWhereToAdd(Node<T,K>* root, K key){
+    if (root->key < key && (root->rightSon)) return FindWhereToAdd(root->rightSon, key);
+    if (root->key > key && (root->leftSon)) return FindWhereToAdd(root->leftSon, key);
+    return root;
+}
+
+
+template <class T, class K>
+static Node<T,K>* FindKey (Node<T,K>* root, K key) {
+    while (root != NULL){
+        if (root->key > key){
+            root = root->leftSon;
+        } else if (root->key < key){
+            root = root->rightSon;
+        } else{
+            return root;
+        }
+    }
+    return NULL;
+}
+
+template <class T,class K>
+static Node<T,K>* InsertNode (Node<T,K>* root ,const T& data, const K& key) {
+    Node<T,K>* temp;
+    if (root == NULL){
+        return new Node<T, K>(data, key);
+    } else if (root->key > key) {
+        temp = InsertNode(root->leftSon, data, key);
+        root->height++;
+        root->leftSon = temp;
+        temp->parent = root;
+
+    } else if (root->key < key){
+        temp = InsertNode(root->rightSon, data, key);
+        root->height++;
+        root->rightSon = temp;
+        temp->parent = root;
+    }
+    return Balance(root);
+}
+
+template <class T, class K>
+int AVL<T,K>::GetSize() {
+    return size;
+}
+
+template <class T, class K>
+void AVL<T,K>::Insert(const T& data, const K& key) {
+    if (contains(key)) {
+        throw KeyAlreadyExist();
+    }
+    this->head = InsertNode(head, data, key);
+    this->head->parent = NULL;
+    size++;
+}
+
+
+template <class T, class K>
+bool AVL<T,K>::contains(const K key) {
+    Node<T,K>* node = FindKey(head, key);
+    if (!node){
+        return false;
+    }
+    return true;
+}
+
+template <class T, class K>
+T& AVL<T,K>::Get(const K key) {
+    Node<T,K>* node = FindKey(head, key);
+    return node->data;
+}
+template <class T, class K>
+Node<T,K>* AVL<T,K>::GetNode(const K key) {
+    Node<T,K>* node = FindKey(head, key);
+    return node;
+}
+template <class T, class K>
+void AVL<T,K>::remove(const K key){
+    Node<T,K>* erase = FindKey(head, key);
+    if (!(erase)) return;
+    Node<T,K>* parent = erase->parent;
+    int right = CheckSons(erase);
+    int parentsSon = CheckWhichSonForParent(erase);
+    if (right == -1){
+        if (parentsSon != -1){
+            (parentsSon == 1) ? parent->rightSon = NULL : parent->leftSon = NULL;
+        }
+        if (parentsSon == -1 && size == 1) head = NULL;
+    }
+    else if (right != 2){
+        if (parentsSon == -1){
+            head = (erase->rightSon) ? erase->rightSon : erase->leftSon;
+            head->parent=NULL;
+        }
+        else if (parentsSon == 1){
+            if (right == 1){
+                erase->parent->rightSon = erase->rightSon;
+                erase->rightSon->parent = erase->parent;
+            }
+            else{
+                erase->parent->rightSon = erase->leftSon;
+                erase->leftSon->parent = erase->parent;
+            }
+        }
+        else{
+            if (right == 1){
+                erase->parent->leftSon = erase->rightSon;
+                erase->rightSon->parent = erase->parent;
+            }
+            else{
+                erase->parent->leftSon = erase->leftSon;
+                erase->leftSon->parent = erase->parent;
+            }
+        }
+    }
+    else{
+        Node<T,K>* alternative = erase->rightSon;
+        while (alternative->leftSon) alternative = alternative->leftSon;
+        SwitchNodes(erase, alternative);
+        parent = erase->parent;
+        parentsSon = CheckWhichSonForParent(erase);
+        if (parentsSon == 1) {
+            erase->parent->rightSon = erase->rightSon;
+           if(erase->rightSon) erase->rightSon->parent = erase->parent;
+        }
+        else{
+            erase->parent->leftSon = erase->rightSon;
+           if(erase->rightSon) erase->rightSon->parent = erase->parent;
+        }
+        if (erase == head){
+            head = alternative;
+            head->parent = NULL;
+        }
+    }
+    Node<T, K>* tmp;
+    while (parent != NULL){
+        tmp = Balance(parent);
+        if (parent == head)
+            head = tmp;
+        parent = parent->parent;
+    }
+    delete erase;
+    size--;
+};
+
+static int power(int h){
+    int base = 1;
+    while (h){
+        base *= 2;
+        h--;
+    }
+    return base;
+}
+
+template <class T, class K>
+void EmptyTreeAux(Node<T,K>** head,int h, Node<T,K>* dummy ){
+    if (!(*head) && h > 0) {
+        (*head) = new Node<T, K>(dummy->data, dummy->key);
+        (*head)->parent = NULL;
+    }
+    if (h > 1) {
+        (*head)->leftSon = new Node<T, K>(dummy->data, dummy->key);
+        (*head)->rightSon = new Node<T, K>(dummy->data, dummy->key);
+        (*head)->leftSon->parent = (*head);
+        (*head)->rightSon->parent = (*head);
+        EmptyTreeAux((&(*head)->rightSon), h - 1, dummy);
+        EmptyTreeAux(&((*head)->leftSon), h - 1, dummy);
+        Fixheight((*head));
+        return;
+    }
+}
+
+static int GetLog (int size){
+    int h = 0;
+    if (size == 0) return 0;
+    while (size > 1){
+        size /= 2;
+        h++;
+    }
+    return h+1;
+}
+
+template <class T, class K>
+void AVL<T, K>::EmptyTree(int size, Node<T,K>* dummy) {
+    int h = GetLog(size);
+    EmptyTreeAux(&head, h, dummy);
+    this->size = power(h)-1;
+}
+
+template<class T, class K>
+class DeleteNodes {
+    int numOfNodes;
+public:
+
+    DeleteNodes(int n) : numOfNodes(n) {}
+    ~DeleteNodes() = default;
+    bool operator() (Node<T, K>* node){
+        if (node->rightSon == NULL && node->leftSon == NULL
+            && numOfNodes != 0) {
+            int side = CheckWhichSonForParent(node);
+            if (side == 1){
+                node->parent->rightSon = NULL;
+            } else {
+                node->parent->leftSon = NULL;
+            }
+            delete node;
+            numOfNodes--;
+            return true;
+        }
+        return false;
+    }
+};
+
+template <class T,class K>
+void AVL<T, K>::BuildAlmostComplete(int s, Node<T,K>* dummy) {
+    EmptyTree(s, dummy);
+    int numToDelete = this->size - s;
+    DeleteNodes<T, K> deleteExtraNode(numToDelete);
+    ReverseInOrder(deleteExtraNode);
+}
+
+#endif //WET1_AVLTREE_H
+
---
+++ exceptions.h
@@ -0,0 +1,20 @@
+
+#ifndef WET1_EXEPTIONS_H
+#define WET1_EXEPTIONS_H
+
+#include <stdexcept>
+
+namespace Wet1{
+
+    class Wet1Exeption {};
+
+    namespace AVL {
+
+        class AVLexeption : public  Wet1Exeption{};
+        class IllegalArgument : public AVLexeption{};
+        class KeyAlreadyExist : public AVLexeption{};
+        class KeyDoesntExist : public AVLexeption{};
+    }
+}
+
+#endif //WET1_EXEPTIONS_H
